83f6c9a6d1a7a218a22c144c3ba375930ec90e68
alamincmt6615 <alamincmt6615@gmail.com>
2020-03-13 10:27:33 +0600

    create just three fragment and one activity

---
+++ app/src/main/java/alamin/c/islamicapp/fragment/BlessingFragment.java
@@ -0,0 +1,29 @@
+package alamin.c.islamicapp.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import alamin.c.islamicapp.R;
+
+public class BlessingFragment extends Fragment {
+
+
+    public BlessingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blessing, container, false);
+    }
+
+}
---
+++ app/src/main/java/alamin/c/islamicapp/fragment/SubjectFragment.java
@@ -0,0 +1,32 @@
+package alamin.c.islamicapp.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import alamin.c.islamicapp.R;
+
+
+public class SubjectFragment extends Fragment {
+
+    public SubjectFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_subject, container, false);
+    }
+
+
+}
