98fe2184ca8feda4b89f47b556d3f539b0e522a5
alamincmt6615 <alamincmt6615@gmail.com>
2020-03-13 10:35:58 +0600

    add a toast msg

--- app/src/main/java/alamin/c/islamicapp/fragment/BlessingFragment.java
+++ app/src/main/java/alamin/c/islamicapp/fragment/BlessingFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import alamin.c.islamicapp.R;
 
@@ -23,6 +24,7 @@ public class BlessingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Toast.makeText(getActivity(), "ha ha ha ", Toast.LENGTH_SHORT).show();
         return inflater.inflate(R.layout.fragment_blessing, container, false);
     }
 
